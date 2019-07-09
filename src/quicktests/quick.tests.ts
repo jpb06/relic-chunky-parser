c02b45ae24daf379ca753c8176a22c7bf575d956
jpb <jpb.06@outlook.com>
2019-07-09 18:59:31 +0200

    Some dirty quick tests

---
+++ src/quicktests/quick.tests.ts
@@ -0,0 +1,18 @@
+﻿import { RelicChunkyParser } from "..";
+
+let yolo = async () => {
+    try {
+        let r = await RelicChunkyParser.getReplayData(
+            'D:\\SteamGames\\steamapps\\common\\Dawn of War Soulstorm\\Playback\\' +
+            //'Antiga Bay (2) v10.2019-07-06.15-51-23.rec'
+            'Sands of Time (2) v10_06292019-091533.rec'
+        );
+        console.log('modname', r.modName);
+
+        let b = 0;
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
+yolo();
