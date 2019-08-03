d712c67602f90937027715ec164de4ec487cbe4c
jpb <jpb.06@outlook.com>
2019-08-03 06:48:58 +0200

    Lame tests

--- src/quicktests/quick.tests.ts
+++ src/quicktests/quick.tests.ts
@@ -7,7 +7,11 @@ let yolo = async () => {
             //'Antiga Bay (2) v10.2019-07-06.15-51-23.rec'
             'Sands of Time (2) v10_06292019-091533.rec'
         );
-        console.log('modname', r.modName);
+        if (r === undefined) {
+            console.log('Result is undefined');
+        } else {
+            console.log('modname', r.modName);
+        }
 
         let b = 0;
     } catch (err) {
